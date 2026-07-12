91de9b17d34db82e896cadbf88b4d8c497799747
Willy Yeremi <willyyeremi@gmail.com>
2026-07-12 21:59:38 +0700

    ggg

--- initiate.bat
+++ initiate.bat
@@ -5,6 +5,8 @@ docker build -f dockerfile.ranger_usersync -t apache/ranger-custom-usersync:2.8.
 
 kubectl apply -f namespace.yaml
 
+@REM tier 1
+
 kubectl apply -f ./rdbms/postgresql-metadata.yaml -n rdbms
 kubectl apply -f ./rdbms/postgresql-ops.yaml -n rdbms
 kubectl wait --for=condition=ready pod -l app=postgres -n rdbms --timeout=60s
@@ -14,6 +16,14 @@ kubectl wait --for=condition=ready pod -l app=minio -n minio --timeout=60s
 kubectl apply -f minio_initiate_task.yaml -n minio
 @REM kubectl logs -n minio job/minio-bucket-init   
 
+kubectl apply -f opensearch_secret.yaml -n opensearch
+kubectl apply -f opensearch_configmap_common.yaml -n opensearch
+kubectl apply -f opensearch.yaml -n opensearch
+
+@REM tier 2
+
+kubectl apply -f ./lldap/lldap.yaml -n lldap
+
 kubectl apply -f airflow_secret.yaml -n airflow
 kubectl apply -f airflow_rbac.yaml -n airflow
 kubectl apply -f airflow_configmap_common.yaml -n airflow
@@ -22,18 +32,15 @@ kubectl apply -f airflow_initiate_task.yaml -n airflow
 kubectl apply -f airflow_worker.yaml -n airflow
 kubectl apply -f airflow.yaml -n airflow
 
-kubectl apply -f lldap.yaml -n lldap
-
-kubectl apply -f opensearch_secret.yaml -n opensearch
-kubectl apply -f opensearch_configmap_common.yaml -n opensearch
-kubectl apply -f opensearch.yaml -n opensearch
+@REM ---
 
 @REM kubectl port-forward -n rdbms svc/postgres-metadata 5432:5432
 @REM kubectl port-forward -n rdbms svc/postgres-ops 5431:5432
 @REM kubectl port-forward -n minio svc/minio 9001:9001
-@REM kubectl port-forward -n airflow svc/airflow-api-server 8080:8080
-@REM kubectl port-forward -n lldap svc/lldap 17170:17170
 @REM kubectl port-forward -n opensearch svc/opensearch-dashboard 5601:5601
+@REM kubectl port-forward -n lldap svc/lldap 17170:17170
+@REM kubectl port-forward -n airflow svc/airflow-api-server 8080:8080
+
 
 
 kubectl logs -n rdbms statefulset.apps/postgres-metadata
