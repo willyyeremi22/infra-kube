bff9a30fc4754a6fa031ee101532d9ba93d4f94f
Willy Yeremi <willyyeremi22@gmail.com>
2026-07-23 18:51:27 +0700

    update

--- initiate.bat
+++ initiate.bat
@@ -1,21 +1,21 @@
-@REM kubectl apply -f namespace.yaml
+kubectl apply -f namespace.yaml
 
-@REM @REM tier 1
+@REM tier 1
 
-@REM kubectl apply -f ./rdbms/postgresql-metadata.yaml -n rdbms
-@REM kubectl wait --for=condition=ready pod -l app=postgres-metadata -n rdbms --timeout=60s
+kubectl apply -f ./rdbms/postgresql-metadata.yaml -n rdbms
+kubectl wait --for=condition=ready pod -l app=postgres-metadata -n rdbms --timeout=60s
 
-@REM kubectl apply -f ./minio/minio.yaml -n minio
-@REM kubectl wait --for=condition=ready pod -l app=minio -n minio --timeout=60s
-@REM kubectl apply -f ./minio/minio_initiate_task.yaml -n minio
-@REM @REM kubectl logs -n minio job/minio-bucket-init   
+kubectl apply -f ./minio/minio.yaml -n minio
+kubectl wait --for=condition=ready pod -l app=minio -n minio --timeout=60s
+kubectl apply -f ./minio/minio_initiate_task.yaml -n minio
+@REM kubectl logs -n minio job/minio-bucket-init   
 
-kubectl delete -f ./airflow/airflow_gitsync.yaml -n airflow
-kubectl delete -f ./airflow/airflow_secret.yaml -n airflow
-kubectl delete -f ./airflow/airflow_rbac.yaml -n airflow
-kubectl delete -f ./airflow/airflow_configmap_common.yaml -n airflow
-kubectl delete -f ./airflow/airflow_initiate_task.yaml -n airflow
-@REM kubectl wait --for=condition=ready pod -l app=airflow-init -n airflow --timeout=60s
+kubectl apply -f ./airflow/airflow_gitsync.yaml -n airflow
+kubectl apply -f ./airflow/airflow_secret.yaml -n airflow
+kubectl apply -f ./airflow/airflow_rbac.yaml -n airflow
+kubectl apply -f ./airflow/airflow_configmap_common.yaml -n airflow
+kubectl apply -f ./airflow/airflow_initiate_task.yaml -n airflow
+kubectl wait --for=condition=ready pod -l app=airflow-init -n airflow --timeout=60s
 @REM kubectl logs -n airflow job/airflow-init
-kubectl delete -f ./airflow/airflow_worker.yaml -n airflow
-kubectl delete -f ./airflow/airflow.yaml -n airflow
+kubectl apply -f ./airflow/airflow_worker.yaml -n airflow
+kubectl apply -f ./airflow/airflow.yaml -n airflow
